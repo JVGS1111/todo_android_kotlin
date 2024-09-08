90c6ba8a580266299f7ef686ef46927b99d32fc8
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-09-08 16:11:10 -0300

    add telas login

--- app/src/main/java/com/guerber/taskapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/guerber/taskapp/ui/auth/LoginFragment.kt
@@ -5,8 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.guerber.taskapp.databinding.FragmentLoginBinding
-
+import com.guerber.taskapp.R
 
 class LoginFragment : Fragment() {
 
@@ -21,6 +22,22 @@ class LoginFragment : Fragment() {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.initListeners()
+    }
+
+    private fun initListeners(){
+        binding.btnRegister.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        binding.btnRecovery.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_recoveryAccountFragment)
+        }
     }
 
     override fun onDestroyView() {
